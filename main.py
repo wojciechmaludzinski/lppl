783b5fc89fe27a14ed21440de95f40ff716f26ea
wojciech.maludzinski <wojtekmal7@gmail.com>
2022-05-17 16:33:15 +0200

    small diffs main

--- main.py
+++ main.py
@@ -41,6 +41,7 @@ def fit_classifier(df, length):
 
     classifier.fit(x_train, y_train, x_test, y_test, y_true)
     prediction = classifier.predict(x_test, y_true, x_train, y_train, y_test)
+    print(prediction)
 
     #VISUALISATION
     # classifier.visualize_filter(x_train, y_train)
@@ -52,48 +53,45 @@ def fit_classifier(df, length):
     print(pred_beautifier(classifier.predicting(df_btc_first)))
 
 
-
 if __name__ == '__main__':
     # os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
     output_directory = os.getcwd()
 
-    # columns_used = 200  # out of length 999
-    # print('200 columns used')
     columns_array = [500]
 
-    # for i in range(len(columns_array)):
-    #     print(f"clean with {columns_array[i]} columns")
-    #     df_clean = pd.read_csv("data\\arrays.csv", header=None)
-    #     df_clean = df_clean.sample(frac=1).reset_index(drop=True)
-    #     df_clean = df_clean.iloc[:, :columns_array[i]]
-    #     fit_classifier(df_clean, columns_array[i])
+    for i in range(len(columns_array)):
+        print(f"clean with {columns_array[i]} columns")
+        df_clean = pd.read_csv("data\\array_500.csv", header=None)
+        df_clean = df_clean.sample(frac=1).reset_index(drop=True)
+        df_clean = df_clean.iloc[:, :columns_array[i]]
+        fit_classifier(df_clean, columns_array[i])
 
-    # for i in range(len(columns_array)):
-    #     print(f"noise1 with {columns_array[i]} columns")
-    #     df = pd.read_csv("data\\df_noise.csv", header=None)
-    #     df = df.sample(frac=1).reset_index(drop=True)
-    #     df = df.iloc[: , :columns_array[i]]
-    #     fit_classifier(df)
-    #
+    for i in range(len(columns_array)):
+        print(f"noise1 with {columns_array[i]} columns")
+        df = pd.read_csv("data\\df_noise.csv", header=None)
+        df = df.sample(frac=1).reset_index(drop=True)
+        df = df.iloc[:, :columns_array[i]]
+        fit_classifier(df, columns_array[i])
+
     for i in range(len(columns_array)):
         print(f"noise2 with {columns_array[i]} columns")
         df2 = pd.read_csv("data\\df_noise2.csv", header=None)
         df2 = df2.sample(frac=1).reset_index(drop=True)
-        df2 = df2.iloc[: , :columns_array[i]]
+        df2 = df2.iloc[:, :columns_array[i]]
         fit_classifier(df2, columns_array[i])
 
     for i in range(len(columns_array)):
         print(f"noise3 with {columns_array[i]} columns")
         df3 = pd.read_csv("data\\df_noise3.csv", header=None)
         df3 = df3.sample(frac=1).reset_index(drop=True)
-        df3 = df3.iloc[: , :columns_array[i]]
+        df3 = df3.iloc[:, :columns_array[i]]
         fit_classifier(df3, columns_array[i])
 
     for i in range(len(columns_array)):
         print(f"noise4 with {columns_array[i]} columns")
         df4 = pd.read_csv("data\\df_noise4.csv", header=None)
         df4 = df4.sample(frac=1).reset_index(drop=True)
-        df4 = df4.iloc[: , :columns_array[i]]
+        df4 = df4.iloc[:, :columns_array[i]]
         fit_classifier(df4, columns_array[i])
 
 
